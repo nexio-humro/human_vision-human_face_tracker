44a9623663c660dfe82dcf889625d3ac504d5ef6
Kamil <kamil.kuklinski@nexio.pl>
2021-05-20 14:56:36 +0200

    add changes from PP visit

--- scripts/listener.py
+++ scripts/listener.py
@@ -7,12 +7,29 @@ import rospy
 from std_msgs.msg import String
 
 from face_tracker.face_tracker import FaceTracker
+from std_msgs.msg import Float32
+
+pub = rospy.Publisher('/ll_controller/servo_yaw/command', Float32, queue_size=10)
+pozycja = 0
 
 def callback(data):
-    rospy.loginfo(f'Received coordinates ({data.x:.2f}, {data.y:.2f}, {data.z:.2f})')
+    rospy.loginfo("helo")
+  #  rospy.loginfo(f'Received coordinates ({data.x:.2f}, {data.y:.2f}, {data.z:.2f})')
     angle = FaceTracker().calculate_rotation_angle(data.x, data.z)
     angle_degrees = radians_to_degrees(angle)
-    rospy.loginfo(f'Calculated angle = {angle:.2f} radians, angle = {angle_degrees:.2f} degrees')
+    global pozycja
+    
+	
+    if not math.isnan(angle_degrees):
+        pozycja += angle_degrees * 0.1
+	if pozycja > 60:
+            pozycja = 60
+
+        if pozycja < -60:
+            pozycja =-60
+    
+    pub.publish(pozycja)
+  #  rospy.loginfo(f'Calculated angle = {angle:.2f} radians, angle = {angle_degrees:.2f} degrees')
 
 def radians_to_degrees(angle):
     return angle/(2*math.pi) * 360
